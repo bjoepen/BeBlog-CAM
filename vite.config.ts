cae17350eaf6ebc6f61808827790255e56ac0fd2
bjoepen <bernd.joepen@t-online.de>
2026-08-17 16:20:47 +0200

    Build BeBlog CAM 001A foundation

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  plugins: [svelte()],
+  clearScreen: false,
+  server: {
+    strictPort: true,
+    port: 1420,
+    watch: { ignored: ['**/src-tauri/**'] }
+  }
+});
