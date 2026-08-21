c25e12d5a1e35ff22f2536b3d99cf5256c3d547c
bjoepen <bernd.joepen@t-online.de>
2026-08-21 17:26:37 +0200

    Setze GRBL als expliziten Standard Postprozessor

--- src/lib/postProcessorStore.ts
+++ src/lib/postProcessorStore.ts
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store';
 import type { PostProcessorId } from './postprocessors';
 
-export const postProcessorStore=writable<PostProcessorId>('generic');
+export const postProcessorStore=writable<PostProcessorId>('grbl');
