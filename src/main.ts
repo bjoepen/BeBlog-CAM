7048e0fdda754069c3005a83e6eac577f41def66
bjoepen <bernd.joepen@t-online.de>
2026-08-22 08:13:47 +0200

    Mount CAM Floh als echtes persistentes Sidebar Element

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { mount } from 'svelte';
 import App from './App.svelte';
+import CamMascot from './lib/CamMascot.svelte';
 import './app.css';
 import './floh.css';
 
 mount(App, { target: document.getElementById('app')! });
+
+requestAnimationFrame(() => {
+  const rail = document.querySelector<HTMLElement>('.rail');
+  if (rail && !rail.querySelector('.cam-mascot')) {
+    mount(CamMascot, { target: rail });
+  }
+});
